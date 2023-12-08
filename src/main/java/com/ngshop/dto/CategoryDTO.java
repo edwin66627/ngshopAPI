4934980bdcdaa57fdf2d1e7d6c9876356aec7934
Edwin Erley Quevedo Sierra <edwinqs1956@hotmail.com>
2023-12-08 16:36:18 -0500

    Added REST capability to get all categories

--- src/main/java/com/ngshop/dto/CategoryDTO.java
+++ src/main/java/com/ngshop/dto/CategoryDTO.java
@@ -1,2 +1,17 @@
-package com.ngshop.dto;public class CategoryDTO {
+package com.ngshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+    private Long id;
+    private String name;
+    private String color;
+    private String icon;
 }
--- src/main/java/com/ngshop/repository/CategoryRepository.java
+++ src/main/java/com/ngshop/repository/CategoryRepository.java
@@ -1,2 +1,9 @@
-package com.ngshop.repository;public interface CategoryRepository {
+package com.ngshop.repository;
+
+import com.ngshop.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
