c63c67dc2972d645b9c98f2e5be4cc1e5bfeaab1
Edwin Erley Quevedo Sierra <edwinqs1956@hotmail.com>
2024-01-01 21:19:31 -0500

    Basic Security filter chain configuration

---
+++ src/main/java/com/ngshop/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.ngshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                    request.requestMatchers("/auth/login", "/product/list").permitAll()
+                    .anyRequest().authenticated()
+                )
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+
+    }
+}
