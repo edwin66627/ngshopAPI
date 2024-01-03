ecd4b8e1b6ee58d60e3c7cb48e222f09d9c32e73
Edwin Erley Quevedo Sierra <edwinqs1956@hotmail.com>
2024-01-02 21:42:28 -0500

    JWT generation set up * Added authentication filter and set it up

---
+++ src/main/java/com/ngshop/security/JWTAuthenticationFilter.java
@@ -0,0 +1,63 @@
+package com.ngshop.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import static org.apache.commons.lang3.StringUtils.startsWith;
+
+@Component
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    private JWTTokenProvider jwtTokenProvider;
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public JWTAuthenticationFilter(JWTTokenProvider jwtTokenProvider, UserDetailsServiceImpl userDetailsService) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+
+        if(StringUtils.isEmpty(authHeader) || !startsWith(authHeader, "Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        username = jwtTokenProvider.extractUsername(jwt);
+
+        if(StringUtils.isNotEmpty(username) && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if(jwtTokenProvider.isTokenValid(jwt, userDetails)){
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, null,
+                        userDetails.getAuthorities());
+
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                securityContext.setAuthentication(token);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/ngshop/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.ngshop.security;
+
+import com.ngshop.constant.ExceptionMessage;
+import com.ngshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> {
+            return new UsernameNotFoundException(String.format(ExceptionMessage.NO_SUCH_ELEMENT, "User", username));
+        });
+    }
+}
